f247c4071ea22c9dc6fe291886e082de97a85420
Anthony Montesdeoca <anthony_chen97@yahoo.com>
2025-01-28 16:40:04 -0500

    Adding components to my pages

--- ecommerce/src/pages/Products.js
+++ ecommerce/src/pages/Products.js
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import '../styling/Products.css';
+import Header from '../components/Header';
+import NavBar from '../components/NavBar';
+import Footer from '../components/Footer';
 
 const Products = () => {
     const [products, setProducts] = useState([]);
@@ -12,18 +15,24 @@ const Products = () => {
     }, []);
 
     return (
-        <section className="main-container">
-            <div id="products">
-                {products.map((item, index) => (
-                    <div key={index} className="item-container">
-                        <img src={item.image} alt={item.name} />
-                        <h3>{item.name}</h3>
-                        <p>{item.price}</p>
-                        <p>{item.description}</p>
-                    </div>
-                ))}
-            </div>
-        </section>
+        <>
+            <Header />
+            <NavBar />
+            <section className="main-container">
+                <div id="products">
+                    {products.map((item, index) => (
+                        <div key={index} className="item-container">
+                            <img src={item.image} alt={item.name} />
+                            <h3>{item.name}</h3>
+                            <p>{item.price}</p>
+                            <p>{item.description}</p>
+                        </div>
+                    ))}
+                </div>
+            </section>
+            <Footer />
+        </>
+
     );
 };
 
